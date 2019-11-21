6393051f63bc2c68a393ba527d11b6e635db78ce
jeong1233 <57386024+jeong1233@users.noreply.github.com>
2019-11-21 10:31:30 +0900

    Add files via upload

---
+++ cafem/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(".panelBg").hide();
+    $(".panel").click(function(){
+        $(".panelMenu").css("width","70%");
+        $(".panelBg").show();
+    })
+    $(".closeBtn").click(function(){
+        $(".panelMenu").css("width", "0");
+		$(".panelBg").hide();
+    })
+})
+
+$(window).scroll(function(){
+    var sc_top=$(this).scrollTop();
+    
+    if(sc_top>0){
+        $(".logo, #header h1, .panel").addClass('headerTop');
+    }else if(sc_top==0){
+        $(".logo, #header h1, .panel").removeClass('headerTop');
+    }
+})
+
+/*$(window).scroll(function(){
+    var sc_top=$(this).scrollTop();
+    
+    if(sc_top>0){
+        $("#header").css({
+            "position":"fixed",
+            "top":"0",
+            "left":"0",
+            "width":"100%"
+        })
+    }else if(sc_top==0){
+        $("#header").css({
+            "position":"static";
+        })
+    }
+})*/
