b966d3a8067e77bd2b311879658dbd45c4b9e287
jeong1233 <57386024+jeong1233@users.noreply.github.com>
2019-11-26 14:42:02 +0900

    Update main.js

--- onepone/js/main.js
+++ onepone/js/main.js
@@ -4,35 +4,12 @@ $(document).ready(function(){
         $(".panelMenu").css("width","70%");
         $(".panelBg").show();
     })
-    $(".fa-times-circle").click(function(){
-        $(".panelMenu").css("width", "0");
-		$(".panelBg").hide();
-    })
+	$(".fa-times-circle").click(function(){
+			$(".panelMenu").css("width", "0");
+			$(".panelBg").hide();
+	})
+	$(".panelBg").click(function(){
+			$(".panelMenu").css("width", "0");
+			$(".panelBg").hide();
+	})
 })
-
-$(window).scroll(function(){
-    var sc_top=$(this).scrollTop();
-    
-    if(sc_top>0){
-        $(".logo, #header h1, .panel").addClass('headerTop');
-    }else if(sc_top==0){
-        $(".logo, #header h1, .panel").removeClass('headerTop');
-    }
-})
-
-/*$(window).scroll(function(){
-    var sc_top=$(this).scrollTop();
-    
-    if(sc_top>0){
-        $("#header").css({
-            "position":"fixed",
-            "top":"0",
-            "left":"0",
-            "width":"100%"
-        })
-    }else if(sc_top==0){
-        $("#header").css({
-            "position":"static";
-        })
-    }
-})*/
